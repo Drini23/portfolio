19140edbe20c6a7bfed679e100d6ffbb2d5d2dbd
Drini23 <122325981+Drini23@users.noreply.github.com>
2025-01-08 23:32:03 +0100

    re

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,12 +18,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("folio.urls")),
     path("projects/", include("projects.urls")),
     path('api/', include('api.urls')),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT})
 ] 
 
 if settings.DEBUG:
